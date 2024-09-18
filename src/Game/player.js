3830d3fca9311173accef06d7002de6b065fdfcd
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-09-18 19:19:37 +0530

    Adding tests for ship

---
+++ src/Game/player.js
@@ -0,0 +1,60 @@
+export default class Player {
+    constructor(name) {
+        this.name = name;
+        this.board = this.createBoard(10); // Assume 10x10 grid
+        this.ships = [];
+        this.shotHistory = [];
+    }
+
+    createBoard(size) {
+
+    }
+
+    placeShip(ship, position) {
+
+    }
+
+    fireShot(target) {
+
+    }
+
+    isSunk() {
+
+    }
+
+    getShipsLeft() {
+
+    }
+
+    getBoard() {
+
+    }
+
+    getName() {
+
+    }
+
+    getScore() {
+
+    }
+
+    getHealth() {
+
+    }
+
+    isGameOver() {
+
+    }
+
+    getRemainingShots() {
+
+    }
+
+    getGameStatus() {
+
+    }
+
+    getShotHistory() {
+
+    }
+}
---
+++ src/Test/ship.test.js
@@ -0,0 +1,9 @@
+import ship from '../Game/ship.js';
+
+describe('ship class', () => {
+    let ship;
+
+    beforeEach(() => {
+        ship = new ship('Battleship', '4', [0, 0]);
+    });
+})
