71515275ec7f7122500246d5e6f949256a4a8490
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-09-18 23:39:49 +0530

    Completed the player class too.

--- src/Game/player.js
+++ src/Game/player.js
@@ -1,10 +1,9 @@
 import GameBoard from "./gameBoard.js";
-import ship from "./ship.js";
 
 export default class Player {
     constructor(name) {
         this.name = name;
-        this.board = this.createBoard(10); // Assume 10x10 grid
+        this.board = this.createBoard(10); 
         this.ships = [];
         this.shotHistory = [];
     }
@@ -14,50 +13,61 @@ export default class Player {
     }
 
     placeShipPlayer(ship, position) {
-
+        this.board.placeShip(ship, position); 
+        this.ships.push(ship); 
     }
 
     fireShot(target) {
-
+        const hit = this.board.checkHit(target); 
+        this.shotHistory.push({ target, hit }); 
+        return hit; 
     }
 
     isSunk() {
-
+        return this.ships.every(ship => ship.isSunk()); 
     }
 
     getShipsLeft() {
-
+        return this.ships.filter(ship => !ship.isSunk()).length; 
     }
 
     getBoard() {
-
+        return this.board;
     }
 
     getName() {
-
+        return this.name;
     }
 
     getScore() {
-
+        return this.shotHistory.filter(shot => shot.hit).length; 
     }
 
     getHealth() {
-
+        return this.ships.reduce((total, ship) => total + ship.getHealth(), 0); 
     }
 
     isGameOver() {
-
+        return this.getShipsLeft() === 0; 
     }
 
     getRemainingShots() {
-
+        return 100 - this.shotHistory.length; 
     }
 
+    // Returns the current game status
     getGameStatus() {
-
+        return {
+            score: this.getScore(),
+            shipsLeft: this.getShipsLeft(),
+            health: this.getHealth(),
+            remainingShots: this.getRemainingShots(),
+            isGameOver: this.isGameOver(),
+        };
     }
 
+    // Returns the history of all shots made by the player
     getShotHistory() {
-
+        return this.shotHistory;
     }
 }
