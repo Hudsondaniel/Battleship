fe541927f5e892f0a82a7930da0ec55627b61ee3
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-09-18 18:39:06 +0530

    Add new file modules to work on

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import './style.css';
 import './css-reset.css';
+import './Game/computerAI.js';
+import './Game/game.js';
+import './Game/gameBoard.js';
+import './Game/ship.js';
+import './Game/player.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Webpack is working!');
