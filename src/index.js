9f5077f9eddf7991fe0dbccf47923afee802a37e
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-10-01 20:50:17 +0530

    Nav bar done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import './style.css';
-import './css-reset.css';
 import './Game/computerAI.js';
 import './Game/game.js';
 import './Game/gameBoard.js';
 import './Game/ship.js';
 import './Game/player.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Webpack is working!');
-});
+
