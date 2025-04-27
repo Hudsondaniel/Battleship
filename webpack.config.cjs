d20c229cac546e3a6782315bec8579b9205aaf94
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2025-04-27 16:54:24 +0530

    Build error change

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         filename: 'main.js', 
         path: path.resolve(__dirname, 'dist'), 
-        publicPath: '/dist/' 
+        publicPath: ''
     },
     mode: 'development',
     module: {
